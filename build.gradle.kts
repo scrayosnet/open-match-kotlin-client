977501d076badd547cfa3d6ce1f5adbcab858760
Joshua Dean Küpper <admin@joshua-kuepper.de>
2022-01-23 21:02:55 +0100

    bump version number for development to 3.0.1-SNAPSHOT
    Signed-off-by: Joshua Dean Küpper <admin@joshua-kuepper.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val mavenRepositoryToken: String by project
 
 // provide general GAV coordinates
 group = "net.justchunks"
-version = "3.0.0"
+version = "3.0.1-SNAPSHOT"
 description = "Open Match Java Client"
 
 // define the grpc versions for the build
