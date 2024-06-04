0129204b9246a609b1ba2ff5e918c85fb518d9a4
Joshua Dean Küpper <admin@joshua-kuepper.de>
2024-06-04 21:35:11 +0200

    update dependencies and remove pitest

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,15 @@ description = "Open Match Java/Kotlin Client"
 
 // hook the plugins for the builds
 plugins {
-    `java-library`
-    `maven-publish`
-    idea
+    alias(libs.plugins.idea)
+    alias(libs.plugins.javaLibrary)
     alias(libs.plugins.kotlin)
     alias(libs.plugins.dokka)
     alias(libs.plugins.kover)
-    alias(libs.plugins.mavenPublish)
     alias(libs.plugins.protobuf)
     alias(libs.plugins.sonarqube)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.mavenPublish)
 }
 
 // configure the repositories for the dependencies
@@ -144,7 +143,6 @@ sonarqube {
         property("sonar.projectName", "open-match-client")
         property("sonar.projectVersion", version)
         property("sonar.projectDescription", description!!)
-        property("sonar.pitest.mode", "reuseReport")
         property(
             "sonar.kotlin.ktlint.reportPaths",
             "build/reports/ktlint/ktlintKotlinScriptCheck/ktlintKotlinScriptCheck.xml," +
